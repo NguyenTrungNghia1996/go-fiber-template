4fd0891c54dd045938204862ae4c5b5d7a6edce2
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2025-06-16 20:40:09 -0700

    Update user.go

--- models/user.go
+++ models/user.go
@@ -2,7 +2,7 @@
 package models
 
 type User struct {
-	ID       string `json:"id" bson:"_id,omitempty"`
+	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	Username string `json:"username" bson:"username"`
 	Password string `json:"password,omitempty" bson:"password"`
 	Email    string `json:"email" bson:"email"`
